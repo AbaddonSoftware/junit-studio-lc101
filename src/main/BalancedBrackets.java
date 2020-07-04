280a037ce8576b00c24e8fa437e0e73f913a1521
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-07-04 17:17:46 -0400

    "commit for hasBrackets"

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,35 +1,98 @@
 package main;
 
-
 public class BalancedBrackets {
+
+
+//    public static boolean hasBalancedBrackets(String str) {
+//        int open = 0;
+//        int close = 0;
+//        for (char ch : str.toCharArray()) {
+//            if (ch == '[') {
+//                open++;
+//            } else if (ch == ']') {
+//                close++;
+//                if (close > open) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return open == close;
+//    }
+//
+// if (close > open) { return false; } may be changed to if (close != open) to forbid mis-nested brackets
+// such as "[[]]" or "[Tac[o]caT]"
+
+//    public static boolean hasBalancedBrackets(String aString) {
+//        boolean closeBracketLast = aString.lastIndexOf('[') < aString.lastIndexOf(']');
+//        boolean equalBrackets = aString.chars().filter(ch -> ch == '[').count() - aString.chars().filter(ch -> ch==']').count() == 0;
+//        boolean mayHaveNoBrackets = !aString.contains("[") && !aString.contains("]");
+//        return closeBracketLast && equalBrackets || mayHaveNoBrackets;
+//    }
+//
+// The above code allows mis-nested brackets as did the original code.
+
     /**
      * The function BalancedBrackets should return true if and only if
      * the input string has a set of "balanced" brackets.
-     *
+     * <p>
      * That is, whether it consists entirely of pairs of opening/closing
      * brackets (in that order), none of which mis-nest. We consider a bracket
      * to be square-brackets: [ or ].
-     *
+     * <p>
      * The string may contain non-bracket characters as well.
-     *
+     * <p>
      * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
-     *
+     * "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "[]"
+     * <p>
      * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
+     * "[LaunchCode", "Launch]Code[", "[", "]["
      *
-     * @param str - to be validated
+     * @param aString - to be validated
      * @return true if balanced, false otherwise
      */
-    public static boolean hasBalancedBrackets(String str) {
-        int brackets = 0;
-        for (char ch : str.toCharArray()) {
-            if (ch == '[') {
-                brackets++;
-            } else if (ch == ']') {
-                brackets--;
+
+    public static boolean hasBalancedBrackets(String aString) {
+        int bracketsBalanced = 0;
+        for (char character : aString.toCharArray()) {
+            bracketsBalanced += (character == '[') ? 1 : 0;
+            bracketsBalanced -= (character == ']') ? 1 : 0;
+            if (bracketsBalanced < 0 || bracketsBalanced > 1) {
+                return false;
             }
         }
-        return brackets == 0;
+        return bracketsBalanced == 0;
     }
+
+    /**
+     * An overloaded version of {@link #hasBalancedBrackets(String aString)}
+     * that allows the user decide if they wish to allow brackletless values
+     * to return true.
+     *
+     * <p>
+     * When the mustContainBrackets parameter is set to true then strings such
+     * as "", "LaunchCode" will return false.
+     * </p>
+     *
+     * @param aString             - to be validated
+     * @param mustContainBrackets - boolean value indicating whether bracketless
+     *                            strings should return true or false.
+     * @return true if balanced, false otherwise.
+     */
+
+    public static boolean hasBalancedBrackets(String aString, boolean mustContainBrackets) {
+        int bracketsBalanced = 0;
+        if (aString.contains("[") && mustContainBrackets) {
+            return false;
+        }
+        for (char character : aString.toCharArray()) {
+            bracketsBalanced += (character == '[') ? 1 : 0;
+            bracketsBalanced -= (character == ']') ? 1 : 0;
+            if (bracketsBalanced < 0 || bracketsBalanced > 1) {
+                return false;
+            }
+        }
+        return bracketsBalanced == 0;
+    }
+
+
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,66 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void bracketedStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[string]"));
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void bracketsOutOfOrderReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void multipleBracketsOutOfOrderReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][][][[]["));
+    }
+
+    @Test
+    public void doubleBackwardsBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets(("][][")));
+    }
+
+    @Test
+    public void nestedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void onlyLeftBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void noBracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void singleBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void complexStringWithImproperBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[This[is[the[song]that]nex[evd]??"));
+    }
+
+    @Test
+    public void complexStringWithProperBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch[Code[s]RoxoR[s]edoC]hcnuaL]"));
+    }
+
 
 
 }
