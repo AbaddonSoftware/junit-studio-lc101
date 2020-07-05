b6646a6e6ec0e511b17775b37e17f91a5eda3018
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-07-05 02:44:14 -0400

    "added Tests and no binary search"

--- src/main/BonusBinarySearch.java
+++ src/main/BonusBinarySearch.java
@@ -13,18 +13,40 @@ public class BonusBinarySearch {
      * @param searchedNumber - number to search for
      * @return index of search item if it's found, -1 if not found
      */
+//    public static int binarySearch(int[] sortedNumbers, int searchedNumber) {
+//        int left = 0;
+//        int right = sortedNumbers.length - 1;
+//        int mid;
+//        while (left <= right) {
+//            mid = (left + right) / 2;
+//            if (sortedNumbers[mid] < searchedNumber) {
+//                left = mid + 1;
+//            } else if (sortedNumbers[mid] > searchedNumber) {
+//                right = mid - 1;
+//            } else {
+//                return mid;
+//            }
+//        }
+//        return -1;
+//    }
+
     public static int binarySearch(int[] sortedNumbers, int searchedNumber) {
         int left = 0;
         int right = sortedNumbers.length - 1;
-        int mid;
+        int middle;
+        int compareResult;
         while (left <= right) {
-            mid = (left + right) / 2;
-            if (sortedNumbers[mid] < searchedNumber) {
-                left = mid + 1;
-            } else if (sortedNumbers[mid] > searchedNumber) {
-                right = mid - 1;
-            } else {
-                return mid;
+            middle = (left + right) / 2;
+            compareResult = Integer.compare(0, sortedNumbers[middle] - searchedNumber);
+            switch(compareResult) {
+                case 1:
+                    left = middle + 1;
+                    break;
+                case -1:
+                    right = middle - 1;
+                    break;
+                case 0:
+                    return middle;
             }
         }
         return -1;
--- src/test/BinarySearchTest.java
+++ src/test/BinarySearchTest.java
@@ -1,26 +1,46 @@
 package test;
 
 import main.BonusBinarySearch;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class BinarySearchTest {
-    private final int[] sortedNoDuplicates = {0, 1, 2, 3, 4, 8, 55, 82};
-    private final List<Integer> sortedNoDuplicatesAsArrayList = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 8, 55, 82));
+    private final int[] sortedNoDuplicates = {2, 3, 4, 6, 8, 9, 55, 82};
+    private final List<Integer> sortedNoDuplicatesAsArrayList = new ArrayList<>();
+
+    @Before
+    public void loadList() {
+        for(int number : sortedNoDuplicates) {
+         sortedNoDuplicatesAsArrayList.add(number);
+        }
+    }
 
     @Test
     public void returnsAnIndex() {
-        assertEquals(sortedNoDuplicatesAsArrayList.indexOf(2), BonusBinarySearch.binarySearch(sortedNoDuplicates, 2));
+        assertEquals(sortedNoDuplicatesAsArrayList.indexOf(4), BonusBinarySearch.binarySearch(sortedNoDuplicates, 4));
     }
 
     @Test
     public void returnsNegativeOneIfNumberNotFound() {
-        assertEquals(BonusBinarySearch.binarySearch(sortedNoDuplicates, 54), -1);
+        assertEquals(-1, BonusBinarySearch.binarySearch(sortedNoDuplicates, 54));
+    }
+
+    @Test
+    public void locatesIfRightMost() {
+        int length = sortedNoDuplicates.length - 1;
+        int rightMostElement = sortedNoDuplicatesAsArrayList.get(length);
+        assertEquals(sortedNoDuplicatesAsArrayList.indexOf(rightMostElement), BonusBinarySearch.binarySearch(sortedNoDuplicates, 82));
+    }
+
+    @Test
+    public void locatesIfLeftMost() {
+        int leftMostElement = sortedNoDuplicatesAsArrayList.get(0);
+        assertEquals(sortedNoDuplicatesAsArrayList.indexOf(leftMostElement), BonusBinarySearch.binarySearch(sortedNoDuplicates, 2));
     }
 
 }
